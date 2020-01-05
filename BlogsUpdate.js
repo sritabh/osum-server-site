09026706a8b88d43fceaaddc5e3701baa58ea15f
SobyDamn <43126956+SobyDamn@users.noreply.github.com>
2020-01-05 14:12:19 +0530

    Update BlogsUpdate.js

--- BlogsUpdate.js
+++ BlogsUpdate.js
@@ -5,6 +5,6 @@ function load() {
 
                      for(var i = 0;i < myblogsdata.length; i++)
                      {
-                        div.innerHTML = div.innerHTML + "<p style='line-height:1%' class='inner' id="+i+">"+"<a href='https://www.osum-blogs.ml/2019/09/add-chatfilter-and-chatlogs-in.html?m=1'><font size='+1.3' color='brown'>"+"<u><b>"+ myblogsdata[i].name +"</u></b></a>"+"<p><font size='-1' color= 'brown'><a href='chats/index.php'><b>➤View ChatLogs!</b></p></a></font>" + "</font>" + "</p>";
+                        div.innerHTML = div.innerHTML + "<p style='line-height:1%' class='inner' id="+i+">"+"<a href='https://www.osum-blogs.ml/2019/09/add-chatfilter-and-chatlogs-in.html?m=1'><font size='+1.3' color='brown'>"+"<u><b>"+ myblogsdata[i].name +"</u></b></a>"+"<p><font size='-1' color= 'brown'><a href='chats/Chats 2019-09-12.html'><b>➤View ChatLogs!</b></p></a></font>" + "</font>" + "</p>";
                      }
                  }
